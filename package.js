5a42b28398ed7291d00968d2c56497a4ed789ca3
harrisonhunter <hhunter@mit.edu>
2014-10-27 16:48:33 -0400

    fixed version export

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'harrison:papa-parse',
   summary: 'A powerful, in-browser CSV parser for big boys and girls',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/harrisonhunter/papa-parse-meteor.git'
 });
 
@@ -12,5 +12,5 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('harrison:papa-parse');
-  api.addFiles('harrison:papa-parse-tests.js', ['client']);
+  api.addFiles('harrison:papa-parse-tests.js', 'client');
 });
