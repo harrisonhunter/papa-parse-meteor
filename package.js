47665f2c72fb53d105d5428582576caaa5473e38
Harrison Hunter <harrisonhunter47@gmail.com>
2015-05-08 16:56:06 -0400

    removed restriction to client

--- package.js
+++ package.js
@@ -2,17 +2,17 @@ Package.describe({
   name: 'harrison:papa-parse',
   summary: 'A powerful, in-browser CSV parser for big boys and girls',
   git: 'https://github.com/harrisonhunter/papa-parse-meteor.git',
-  version: '1.0.10'
+  version: '1.0.11'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.3");
   api.use("jquery");
-  api.addFiles('papa-parse.js', 'client');
+  api.addFiles('papa-parse.js');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('harrison:papa-parse');
-  api.addFiles('papa-parse-tests.js', 'client');
+  api.addFiles('papa-parse-tests.js');
 });
