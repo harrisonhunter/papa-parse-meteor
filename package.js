9e2274c293e8a19e73074f497dbc19f7a5273371
Harrison Hunter <harrisonhunter47@gmail.com>
2015-05-08 18:30:07 -0400

    made papa parse available on the server by aliasing baby parse

--- package.js
+++ package.js
@@ -2,17 +2,21 @@ Package.describe({
   name: 'harrison:papa-parse',
   summary: 'A powerful, in-browser CSV parser for big boys and girls',
   git: 'https://github.com/harrisonhunter/papa-parse-meteor.git',
-  version: '1.0.11'
+  version: '1.1.0'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom("METEOR@0.9.3");
   api.use("jquery");
-  api.addFiles('papa-parse.js');
+  api.addFiles('papa-parse.js', 'client');
+  api.addFiles('baby-parse.js', 'server');
+  api.export('Papa', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('harrison:papa-parse');
-  api.addFiles('papa-parse-tests.js');
+  api.addFiles('papa-parse-tests.js', 'client');
+  api.addFiles('baby-parse-tests.js', 'server');
+  api.export('Papa', 'server');
 });
